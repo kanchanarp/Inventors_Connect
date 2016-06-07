5859930ea2e846f17479f15ea2752407bb64e971
kanchanarp <kanchana.ruwanpathirana@gmail.com>
2016-06-07 18:04:04 +0530

    login control

--- public/control/loginControl.php
+++ public/control/loginControl.php
@@ -1,12 +1,8 @@
 <?php
-
 include_once "../../controller/userHadler.class.php";
 if(isset($_POST["submit"])){
-    echo "test1";
-    echo "<script>alert('test')</script>";
     $userHandler=new userHadler();
     if(isset($_POST["email"])&&isset($_POST["password"])){
-        echo "test2";
         $userHandler->login($_POST["email"],$_POST["password"]);
     }
 
